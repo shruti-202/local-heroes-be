d5f3c901e033bab9b15d8d0cda944a82be876f27
Shruti <shrutigawande1910@gmail.com>
2023-12-25 21:09:37 +0530

    chore: Modified Auth Validation

---
+++ constants/Validator.js
@@ -0,0 +1,39 @@
+const nameValidator = (name) => {
+  const nameRegex = /^[A-Z][a-z]+(?: [A-Z][a-z]*)?$/;
+  return nameRegex.test(name);
+};
+
+const emailValidator = (email) => {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+};
+
+const usernameValidator = (username) => {
+  const usernameRegex = /^[A-Za-z][A-Za-z0-9_]{7,29}$/;
+  return usernameRegex.test(username);
+};
+
+const passwordValidator = (password) => {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
+};
+
+const titleValidator = (title) => {
+    const titleRegex = /^[A-Z][a-z]*(?: [A-Z][a-z]*)*$/;
+    return titleRegex.test(title);
+  };
+  
+const descriptionValidator = (description) => {
+  const descriptionRegex = /^[A-Z][a-z]*(?: [A-Z][a-z]*(?: [0-9]+)?)*$/;
+  return descriptionRegex.test(description);
+};
+
+module.exports = {
+  nameValidator,
+  emailValidator,
+  usernameValidator,
+  titleValidator,
+  passwordValidator,
+  descriptionValidator,
+};
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,36 +1,87 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: [true,"Empty Name"]
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Empty Name"],
     },
-    phone:{
-        type: String,
-        required: [true,"Empty Phone"]
+    phone: {
+      type: String,
+      required: [true, "Empty Phone"],
     },
-    email:{
-        type: String,
-        required: [true,"Empty Email"]
+    email: {
+      type: String,
+      required: [true, "Empty Email"],
     },
-    username:{
-        type:String,
-        required: [true,"Empty Username"]
+    username: {
+      type: String,
+      required: [true, "Empty Username"],
     },
-    password:{
-        type: String,
-        required: [true,"Empty Password"]
+    password: {
+      type: String,
+      required: [true, "Empty Password"],
     },
-    userType:{
-        type: String,
-        enum: ["PROVIDER","CLIENT"],
-        required: [true,"Empty UserType"]
-    }
-},
-{
-    timestamps: true
-}
-)
+    userType: {
+      type: String,
+      enum: ["PROVIDER", "CLIENT"],
+      required: [true, "Empty UserType"],
+    },
 
-module.exports = mongoose.model('users',UserSchema)
+    availability: {
+      startDate: {
+        type: String,
+        require: false,
+      },
+      endDate: {
+        type: String,
+        require: false
+    },
+    startTime: {
+        type: String,
+        require: false
+    },
+    endTime: {
+        type: String,
+        require: false
+    },
+    },
+
+    services: [
+        {
+            category: {
+                type: String,
+                enum: [
+                    "HOME_SERVICES",
+                    "TECHNOLOGY_AND_ELECTRONICS",
+                    "BEAUTY_AND_GROOMING",
+                    "EDUCATIONAL_SERVICES",
+                    "MISCELLANEOUS_SERVICES",
+                ],
+                require: false
+            },
+            title: {
+                type: String,
+                require: false
+            },
+            description: {
+                type: String,
+                require: false
+            },
+            price: {
+                type: String,
+                require: false
+            },
+        }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Users = mongoose.model("users", UserSchema);
+
+module.exports = Users
+
 
