372a493ffc770ed9ff15ae2ef7747aabbf88c669
Shruti <shrutigawande1910@gmail.com>
2024-03-23 15:46:29 +0530

    fix: CORS urls

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173","https://local-heroes.vercel.app/","https://local-heroes-fe-shruti202s-projects.vercel.app/"],
+    origin: ["http://localhost:5173","https://localheroes.netlify.app/"],
   })
 );
 
